afd211947abb9aae7924e59cd80ebceb59d0cc2a
jecheung9 <jeffcheung424@gmail.com>
2025-07-15 00:53:45 -0700

    home page ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <main className="main">
       <section className="page-container">
-        <h1 className="page-title">Welcome!</h1>
+        <h1 className="page-title">Welcome to Price Wolves!</h1>
         <p className="page-text">
-          This is Home Page!
+          Price Wolves aim to hunt stores with the lowest prices.
         </p>
+        <Link to="/dashboard" className="button">Go to Dashboard</Link>
       </section>
     </main>
   );
